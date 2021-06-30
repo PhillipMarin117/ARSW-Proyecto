9ebdbe27b9a98a7ee8ddafdbab386ba4ff8c930b
Nickolas Marin <nickolas.marin@mail.escuelaing.edu.co>
2021-06-30 17:50:07 -0500

    Proyecto 2do Sprint

---
+++ src/main/java/edu/escuelaing/arsw/app/ProyectoARSW/ProyectoArswApplication.java
@@ -0,0 +1,13 @@
+package edu.escuelaing.arsw.app.ProyectoARSW;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProyectoArswApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProyectoArswApplication.class, args);
+	}
+
+}
---
+++ src/test/java/edu/escuelaing/arsw/app/ProyectoARSW/ProyectoArswApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.escuelaing.arsw.app.ProyectoARSW;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProyectoArswApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
